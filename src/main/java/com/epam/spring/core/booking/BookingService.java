6a43c2404df45440d032dbd270dcc030211e64e7
Hanna Eismant <Hanna_Eismant@epam.com>
2015-10-26 11:42:48 +0300

    Create and implement booking service

---
+++ src/main/java/com/epam/spring/core/booking/BookingService.java
@@ -0,0 +1,18 @@
+package com.epam.spring.core.booking;
+
+import com.epam.spring.core.events.Event;
+import com.epam.spring.core.tickets.Ticket;
+import com.epam.spring.core.users.User;
+
+import java.util.Date;
+import java.util.List;
+
+public interface BookingService {
+
+    Integer getTicketPrice(Event event, Date date, Integer seat, User user);
+
+    // todo: set total price here
+    void bookTicket(User user, Ticket ticket);
+
+    List<Ticket> getTicketsForEvent(Event event, Date date);
+}
---
+++ src/main/java/com/epam/spring/core/booking/BookingServiceImpl.java
@@ -0,0 +1,60 @@
+package com.epam.spring.core.booking;
+
+import com.epam.spring.core.discounts.DiscountService;
+import com.epam.spring.core.events.Event;
+import com.epam.spring.core.events.services.EventService;
+import com.epam.spring.core.tickets.Ticket;
+import com.epam.spring.core.users.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Service
+public class BookingServiceImpl implements BookingService {
+
+    @Autowired
+    private EventService eventService;
+
+    private DiscountService discountService;
+
+    public EventService getEventService() {
+        return eventService;
+    }
+
+    public void setDiscountService(DiscountService discountService) {
+        this.discountService = discountService;
+    }
+
+    public void setEventService(EventService eventService) {
+        this.eventService = eventService;
+    }
+
+    @Override
+    public Integer getTicketPrice(Event event, Date date, Integer seat, User user) {
+        return discountService.getDiscount(user, event, date);
+    }
+
+    @Override
+    public void bookTicket(User user, Ticket ticket) {
+        ticket.user = user;
+        ticket.discountPrice = getTicketPrice(ticket.event, ticket.date, ticket.seat, user);
+    }
+
+
+    @Override
+    public List<Ticket> getTicketsForEvent(Event event, Date date) {
+        List<Ticket> result = new ArrayList<>();
+        List<Ticket> tickets = event.getTickets();
+
+        for (Ticket ticket : tickets) {
+            if (date.equals(ticket.date)) {
+                result.add(ticket);
+            }
+        }
+
+        return result;
+    }
+}
