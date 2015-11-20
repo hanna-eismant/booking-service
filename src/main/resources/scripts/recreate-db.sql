ee76e676d13eb7b581d991d165582467b23f202b
Hanna Eismant <Hanna_Eismant@epam.com>
2015-11-20 10:45:48 +0300

    Add constraints between ticket and user, event

--- src/main/resources/scripts/recreate-db.sql
+++ src/main/resources/scripts/recreate-db.sql
@@ -1,33 +1,41 @@
+DROP TABLE tickets
+IF EXISTS;
+
+CREATE TABLE tickets (
+  id             BIGINT PRIMARY KEY,
+  seat           INTEGER      NOT NULL,
+  is_vip         BOOLEAN      NOT NULL,
+  date           VARCHAR(255) NOT NULL,
+  price          DOUBLE       NOT NULL,
+  discount_price DOUBLE,
+  event_id       INTEGER      NOT NULL,
+  user_id        INTEGER
+);
+
 DROP TABLE users
 IF EXISTS;
 
 CREATE TABLE users (
   id       BIGINT PRIMARY KEY,
-  name     VARCHAR(255),
-  email    VARCHAR(255),
-  birthday VARCHAR(255)
+  name     VARCHAR(255) NOT NULL,
+  email    VARCHAR(255) NOT NULL,
+  birthday VARCHAR(255) NOT NULL
 );
+
+ALTER TABLE users ADD CONSTRAINT user_name UNIQUE (name);
+ALTER TABLE users ADD CONSTRAINT user_email UNIQUE (email);
 
 DROP TABLE events
 IF EXISTS;
 
 CREATE TABLE events (
   id         BIGINT PRIMARY KEY,
-  name       VARCHAR(255),
-  base_price DOUBLE,
-  rating     VARCHAR(255)
+  name       VARCHAR(255) NOT NULL,
+  base_price DOUBLE       NOT NULL,
+  rating     VARCHAR(255) NOT NULL
 );
 
-DROP TABLE tickets
-IF EXISTS;
-
-CREATE TABLE tickets (
-  id             BIGINT PRIMARY KEY,
-  seat           INTEGER,
-  is_vip         BOOLEAN,
-  date           VARCHAR(255),
-  price          DOUBLE,
-  discount_price DOUBLE,
-  event_id       INTEGER,
-  user_id        INTEGER
-);
+ALTER TABLE tickets ADD FOREIGN KEY (event_id) REFERENCES events (id)
+  ON DELETE CASCADE;
+ALTER TABLE tickets ADD FOREIGN KEY (user_id) REFERENCES users (id)
+  ON DELETE SET NULL;
