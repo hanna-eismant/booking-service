b09660982e0b9d60a2831fb2c48249fbab6c94e5
Hanna Eismant <hanna.eismant@gmail.com>
2016-02-16 15:06:55 +0300

    Write test for User Service

--- src/test/java/com/epam/spring/core/users/UserServiceIntegrationTest.java
+++ src/test/java/com/epam/spring/core/users/UserServiceIntegrationTest.java
@@ -2,9 +2,10 @@ package com.epam.spring.core.users;
 
 import com.epam.spring.core.AbstractIntegrationTest;
 import com.epam.spring.core.shared.NotFoundException;
-import org.junit.Before;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
 
 import static com.epam.spring.core.TestConstants.USER_BIRTHDAY;
 import static com.epam.spring.core.TestConstants.USER_EMAIL;
@@ -18,36 +19,70 @@ public class UserServiceIntegrationTest extends AbstractIntegrationTest {
     @Autowired
     private UserService userService;
 
-    private User user;
+    @Test
+    public void testRegister() {
+        User user = userService.register(USER_NAME, USER_EMAIL, USER_BIRTHDAY);
+        assertNotNull("Registered user cannot be null", user);
+        assertNotNull("Registered user should have id", user.getId());
+        assertEquals("Registered user has incorrect name", user.getName(), USER_NAME);
+        assertEquals("Registered user has incorrect email", user.getEmail(), USER_EMAIL);
+        assertEquals("Registered user has incorrect birthday", user.getBirthday(), USER_BIRTHDAY);
+    }
 
-    @Before
-    public void setUp() throws Exception {
-        user = null;
+    @Test(expected = Exception.class)
+    public void testRegisterDuplicate() {
+        userService.register(USER_JANE.getName(), USER_JANE.getEmail(), USER_JANE.getBirthday());
     }
 
     @Test
-    public void testRegister() {
-        user = userService.register(USER_NAME, USER_EMAIL, USER_BIRTHDAY);
-        assertNotNull("Registered user cannot be null", user);
-        assertNotNull("Registered user should have id", user.getId());
-        assertEquals(user.getName(), USER_NAME);
-        assertEquals(user.getEmail(), USER_EMAIL);
-        assertEquals(user.getBirthday(), USER_BIRTHDAY);
+    public void testGetAll() {
+        List<UserEntity> all = userService.getAll();
+        assertNotNull("Found list of users cannot be null", all);
+        assertEquals("Found list of users has incorrect size", 2, all.size());
     }
 
     @Test
     public void testGetByIdRegistered() {
         User userTest = userService.getById(USER_JANE.getId());
+        assertNotNull("Found user cannot be null", userTest);
+        assertEquals("Found user has incorrect id", USER_JANE.getId(), userTest.getId());
+        assertEquals("Found user has incorrect name", USER_JANE.getName(), userTest.getName());
+        assertEquals("Found user has incorrect email", USER_JANE.getEmail(), userTest.getEmail());
+        assertEquals("Found user has incorrect birthday", USER_JANE.getBirthday(), userTest.getBirthday());
+    }
 
-        assertNotNull(userTest);
-        assertEquals(USER_JANE.getId(), userTest.getId());
-        assertEquals(USER_JANE.getName(), userTest.getName());
-        assertEquals(USER_JANE.getEmail(), userTest.getEmail());
-        assertEquals(USER_JANE.getBirthday(), userTest.getBirthday());
+    @Test
+    public void testGetByEmailRegistered() {
+        User userTest = userService.getByEmail(USER_JANE.getEmail());
+        assertNotNull("Found user cannot be null", userTest);
+        assertEquals("Found user has incorrect id", USER_JANE.getId(), userTest.getId());
+        assertEquals("Found user has incorrect name", USER_JANE.getName(), userTest.getName());
+        assertEquals("Found user has incorrect email", USER_JANE.getEmail(), userTest.getEmail());
+        assertEquals("Found user has incorrect birthday", USER_JANE.getBirthday(), userTest.getBirthday());
+    }
+
+    @Test
+    public void testGetByNameRegistered() {
+        User userTest = userService.getByName(USER_JANE.getName());
+        assertNotNull("Found user cannot be null", userTest);
+        assertEquals("Found user has incorrect id", USER_JANE.getId(), userTest.getId());
+        assertEquals("Found user has incorrect name", USER_JANE.getName(), userTest.getName());
+        assertEquals("Found user has incorrect email", USER_JANE.getEmail(), userTest.getEmail());
+        assertEquals("Found user has incorrect birthday", USER_JANE.getBirthday(), userTest.getBirthday());
     }
 
     @Test(expected = NotFoundException.class)
     public void testGetByIdUnregistered() {
+        userService.getById(1L);
+    }
+
+    @Test(expected = NotFoundException.class)
+    public void testGetByEmailUnregistered() {
+        userService.getById(1L);
+    }
+
+    @Test(expected = NotFoundException.class)
+    public void testGetByNameUnregistered() {
         userService.getById(1L);
     }
 }
